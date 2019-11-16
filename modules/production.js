13e19cf015dfeed29c67f117d0be77b6924962d5
sharifh4 <sharifh4@coventry.ac.uk>
2019-11-16 17:40:45 +0000

    some changes to production.js

--- modules/production.js
+++ modules/production.js
@@ -5,9 +5,9 @@ module.exports = class Production {
     constructor(dbName = ':memory:') {
         return (async () => {
             this.db = await sqlite.open(dbName)
-            const sql = 'CREATE TABLE IF NOT EXISTS "movies" ("movie" TEXT PRIMARY KEY, "Poster" LONGBLOB, "Details" TEXT, numberOfSeats INTEGER);'  
-            const sql2 = 'CREATE TABLE IF NOT EXISTS "showingSchedule"("ShowNumber" TEXT PRIMARY KEY, "date" TEXT, "time" TEXT,"movie" TEXT, FOREIGN KEY("movie") REFERENCES "movies"("movie"));'
-          //  'CREATE TABLE IF NOT EXISTS "movieDisplayRoom"("displayRoom" INTEGER PRIMARY KEY, "numberOfSeats" INTEGER,FOREIGN KEY ("ShowNumber") REFERENCES "showingSchedule"(ShowNumber));'
+            const sql = 'CREATE TABLE IF NOT EXISTS "movies" ("movie" TEXT PRIMARY KEY, "Poster" LONGBLOB, "Details" TEXT);'
+            const sql2 = 'CREATE TABLE IF NOT EXISTS "showingSchedule"("ShowNumber" TEXT PRIMARY KEY, "date" TEXT, "time" TEXT,"movie" TEXT, numberOfSeats INTEGER, FOREIGN KEY("movie") REFERENCES "movies"("movie"));'
+            //  'CREATE TABLE IF NOT EXISTS "movieDisplayRoom"("displayRoom" INTEGER PRIMARY KEY, "numberOfSeats" INTEGER,FOREIGN KEY ("ShowNumber") REFERENCES "showingSchedule"(ShowNumber));'
             await this.db.run(sql)
             await this.db.run(sql2)
             return this
@@ -16,31 +16,32 @@ module.exports = class Production {
     async prodDetails(movie) {
         try {
             let sql = `SELECT * FROM movies WHERE movie = "${movie}";`;
-			db.all(sql, (err, rows) => {
-                if(err) console.error(err.message)
-                if(!err) console.log(rows)       
-        })   }
+            db.all(sql, (err, rows) => {
+                if (err) console.error(err.message)
+                if (!err) console.log(rows)
+            })
+        }
         catch (err) {
             throw err
         }
     }
 
-    async showTime(movie){
+    async showTime(movie) {
         try {
             sql = `SELECT * FROM showingSchedule WHERE movie = "${movie}";`;
-			db.all(sql, (err, rows) => {
-                if(err) console.error(err.message)
-                if(!err) console.log(rows)       
+            db.all(sql, (err, rows) => {
+                if (err) console.error(err.message)
+                if (!err) console.log(rows)
             })
         }
-        catch(err){
+        catch (err) {
             throw err
         }
     }
 }
-        
-        
-    
+
+
+
 
 
 
