9f2f038f64196bff904fbacbf87b46ecafa34c8e
beedhs <beedhs@coventry.ac.uk>
2019-11-20 19:38:21 +0000

    Production.spec.js Working on Unit Testing for proDetails.

--- unit tests/production.spec.js
+++ unit tests/production.spec.js
@@ -5,13 +5,24 @@ const Production = require('../modules/production.js')
 
 describe('prodDetails()', () =>
  {
-	test('Select schedule of movie ', async done => {
-		const prodDetails =  new Production('Advatar')
-		expect(prodDetails).toBe("String")
-    })
-    
 
+	test('No Films Within dB', async done => {
+		expect.assertions(1)
+		const production = await new Production()
+		await expect( production.prodDetails('The delusion',"" ))
+			.rejects.toEqual( Error('missing Movie') ) 
+		done()
+	})
 
-
-
+	test('Valid Entry of Movie in dB', async done => {
+		expect.assertions(1)
+		const production = await new Production()
+		const prodDetails = await production.prodDetails('Advatar')
+		expect(prodDetails).toBe(true)
+		done()
+	})
 })
+
+ 
+
+
