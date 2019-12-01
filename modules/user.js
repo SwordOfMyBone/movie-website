fa4ade6b1a8d7697ec0a680f3434c5bf1792bcc4
Mazin <mazinalhaddad05@gmail.com>
2019-12-01 15:42:24 +0000

    MISSING QUOTATION MARK

--- modules/user.js
+++ modules/user.js
@@ -53,7 +53,7 @@ module.exports = class User {
 							if (data.records !== 0) throw new Error(`username "${user}" already in use`)
 							pass = await bcrypt.hash(pass, saltRounds)
 
-							sql = `INSERT INTO users(user, pass, email) VALUES("${user}", "${pass}", "${email});`
+							sql = `INSERT INTO users(user, pass, email) VALUES("${user}", "${pass}", "${email}");`
 							await this.db.run(sql)
 							sql = `SELECT * FROM users WHERE user="${user}";`
 							let id = ''
